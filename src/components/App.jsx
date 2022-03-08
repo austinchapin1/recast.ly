77e04613cdbde43b39c3169571576c499a6a39dc
Austin Chapin <austinchapin04@gmail.com>
2022-03-07 18:15:11 -0800

    Video list displaying

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import ExampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -11,6 +14,7 @@ var App = () => (
       </div>
       <div className="col-md-5">
         <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={ExampleVideoData}/>
       </div>
     </div>
   </div>
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,31 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+
+
+class VideoListEntry extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render () {
+    return (
+
+      <div className="video-list-entry media">
+        <h1>{console.log('test')}</h1>
+        <div className="media-left media-middle">
+          <img className="media-object" src={this.props.movie.snippet.thumbnails.default.url} alt="" />
+        </div>
+        <div className="media-body">
+          <div className="video-list-entry-title">{this.props.movie.snippet.title}</div>
+          <div className="video-list-entry-detail">{this.props.movie.snippet.description}</div>
+        </div>
+      </div>
+
+    );
+  }
+
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -19,3 +36,31 @@ VideoListEntry.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default VideoListEntry;
+
+
+// class GroceryListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       done: false,
+//       bold: false
+//     };
+//   }
+
+//   render() {
+
+//     var style = {
+//       textDecoration: this.state.done ? 'line-through' : 'none',
+//     };
+//     if (this.state.bold) {
+//       var weight = '900';
+//     } else {
+//       var weight = 'normal';
+//     }
+
+
+//     return(
+//       <li style={style}>{this.props.grocery}</li>
+//     );
+//   }
+// }
